b961d06727e52c9b9b6cf6c0d2d9a393b2a1ab50
Abdelrahman_Malek <amm2592000@gmail.com>
2023-06-19 20:34:31 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProductsModule } from './products/products.module';
+import { CartModule } from './cart/cart.module';
+import { HttpClientModule } from '@angular/common/http';
+import { CommentsModule } from './comments/comments.module';
+import { CommentComponent } from './comments/components/comment/comment.component';
+import { CommentsComponent } from './comments/components/comments/comments.component';
+//import { CommentFormComponent } from './comments/components/comment-form/comment-form.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    NgbModule,
+    ProductsModule,
+    CartModule,
+    HttpClientModule,
+    CommentsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/products/products.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AllProductsComponent } from './components/all-products/all-products.component';
+import { DetailsComponent } from './components/details/details.component';
+import { SharedModule } from '../shared/shared.module';
+import { ProductComponent } from './components/product/product.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { CommentsModule } from '../comments/comments.module';
+
+
+@NgModule({
+  declarations: [
+    AllProductsComponent,
+    DetailsComponent,
+    ProductComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    FormsModule,
+    RouterModule,
+    CommentsModule
+  ]
+})
+export class ProductsModule { }
